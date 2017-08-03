3806d8ac24b4e4e87c8103eb76a2056b446ff738
wdyoschina <123wangdongyun@163.com>
2017-08-03 14:40:06 +0800

    #108 check mychange address owned sending account or not

--- src/lib/explorer/extensions/commands/private_send.cpp
+++ src/lib/explorer/extensions/commands/private_send.cpp
@@ -113,6 +113,8 @@ console_result sendmore::invoke (std::ostream& output,
     blockchain.is_account_passwd_valid(auth_.name, auth_.auth);
     if (!blockchain.is_valid_address(argument_.mychange_address))
         throw toaddress_invalid_exception{std::string("invalid address!") + argument_.mychange_address};
+    if (!blockchain.get_account_address(auth_.name, argument_.mychange_address))
+        throw argument_legality_exception{argument_.mychange_address + std::string(" not owned to ") + auth_.name};
     // receiver
     receiver_record record;
     std::vector<receiver_record> receiver;
@@ -659,6 +661,8 @@ console_result sendassetmore::invoke (std::ostream& output,
     // mychange address check
     if (!blockchain.is_valid_address(argument_.mychange_address))
         throw toaddress_invalid_exception{"invalid mychange address parameter!"};
+    if (!blockchain.get_account_address(auth_.name, argument_.mychange_address))
+        throw argument_legality_exception{argument_.mychange_address + std::string(" not owned to ") + auth_.name};
 
     // receiver
     receiver_record record;
