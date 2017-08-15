02ce6e636903ae65db4d176485c37ea5160e5594
919395 <hhw_505@aliyun.com>
2017-08-14 23:23:59 -0700

    add new transaction version and fix bug

--- include/metaverse/explorer/extensions/base_helper.hpp
+++ include/metaverse/explorer/extensions/base_helper.hpp
@@ -118,7 +118,7 @@ public:
 	virtual void populate_change() = 0; 
 
 	virtual void populate_tx_header(){
-	    tx_.version = transaction_version::max_version - 1;
+	    tx_.version = transaction_version::check_output_script;
 	    tx_.locktime = 0;
 	};
 
@@ -239,6 +239,10 @@ public:
     void sync_fetchutxo (const std::string& prikey, const std::string& addr) override;
     attachment populate_output_attachment(receiver_record& record) override;
     uint64_t get_volume() { return volume_; };
+    void populate_tx_header(){
+        tx_.version = transaction_version::asset_secondissue_and_frozen;
+        tx_.locktime = 0;
+    };
 
 private:
     uint64_t volume_;
