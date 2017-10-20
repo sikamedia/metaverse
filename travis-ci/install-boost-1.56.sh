171a3d2c0b3edcdf10b0bb44bb3417e633f175eb
sikamedia <simonwantao@yahoo.com>
2017-10-20 14:36:26 +0200

    disable sudo and disable to show extract zip file.

--- travis-ci/install-boost-1.56.sh
+++ travis-ci/install-boost-1.56.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ex
 wget -c http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.gz
-tar -xzvf boost_1_56_0.tar.gz
+tar -xzf boost_1_56_0.tar.gz
 cd boost_1_56_0 && ./bootstrap.sh && sudo ./b2 install
