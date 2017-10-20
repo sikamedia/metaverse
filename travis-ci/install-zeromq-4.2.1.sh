e42281f4305d1a1f0962d1a342fbe99a0f189187
sikamedia <simonwantao@yahoo.com>
2017-10-21 01:36:07 +0200

    try to build with more threads.

--- travis-ci/install-zeromq-4.2.1.sh
+++ travis-ci/install-zeromq-4.2.1.sh
@@ -2,4 +2,4 @@
 set -ex
 wget https://github.com/zeromq/libzmq/releases/download/v4.2.1/zeromq-4.2.1.tar.gz
 tar -xzf zeromq-4.2.1.tar.gz
-cd zeromq-4.2.1 && ./autogen.sh && ./configure && make -j4 && sudo make install && sudo ldconfig
+cd zeromq-4.2.1 && ./autogen.sh && ./configure && make -j8 && sudo make install && sudo ldconfig
