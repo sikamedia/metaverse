2bb11be9a3ade1f15ac7aa64630a8c1525123a29
sikamedia <simonwantao@yahoo.com>
2017-10-20 13:46:25 +0200

    correct build script for boost install. https://github.com/mvs-org/metaverse/issues/164

--- travis-ci/install-boost-1.56.sh
+++ travis-ci/install-boost-1.56.sh
@@ -2,4 +2,4 @@
 set -ex
 wget -c http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.gz
 tar -xzvf boost_1_56_0.tar.gz
-cd boost_1_56_0 && ./configure --prefix=/usr && make && sudo make install
+cd boost_1_56_0 && ./bootstrap.sh && sudo ./b2 install
