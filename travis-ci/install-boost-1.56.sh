b5dd1b2395e8c325b925b203604e0e9faee0ba98
sikamedia <simonwantao@yahoo.com>
2017-10-20 22:32:32 +0200

    correct error syntax.

--- travis-ci/install-boost-1.56.sh
+++ travis-ci/install-boost-1.56.sh
@@ -2,4 +2,4 @@
 set -ex
 wget -c http://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.gz
 tar -xzf boost_1_56_0.tar.gz
-cd boost_1_56_0 && ./bootstrap.sh && ./b2 -j 4 && sudo ./b2 install && && sudo ldconfig
+cd boost_1_56_0 && ./bootstrap.sh && ./b2 -j 4 && sudo ./b2 install && sudo ldconfig
