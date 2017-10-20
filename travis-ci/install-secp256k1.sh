aef30bb595be5ad05a9f0d32bd5b4655e6cdead1
sikamedia <simonwantao@yahoo.com>
2017-10-20 16:23:25 +0200

    build metaverse core. https://github.com/mvs-org/metaverse/issues/164

--- travis-ci/install-secp256k1.sh
+++ travis-ci/install-secp256k1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 git clone https://github.com/mvs-live/secp256k1
-cd secp256k1 && ./autogen.sh && ./configure --enable-module-recovery && make && sudo make install && sudo ldconfig
+cd secp256k1 && ./autogen.sh && ./configure --enable-module-recovery && make -j4 && sudo make install && sudo ldconfig
